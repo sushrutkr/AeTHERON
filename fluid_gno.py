80d0fd5abbf81c4b7de4800bee689539add56353
sushrutkr <sushrut98@gmail.com>
2024-12-01 15:30:55 -0500

    Initial commit with cleaned state

---
+++ fluid_gno.py
@@ -0,0 +1,211 @@
+# Standard library imports
+import random
+from timeit import default_timer
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from utilities import *
+from model.spectralGraphNetwork import *
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def set_seed(seed):
+	torch.manual_seed(seed)
+	torch.cuda.manual_seed_all(seed)
+	np.random.seed(seed)
+	random.seed(seed)
+	torch.backends.cudnn.deterministic = True
+	torch.backends.cudnn.benchmark = False
+
+def ijk_to_index(i, j, k, n_y, n_z):
+    return i * (n_y * n_z) + j * n_z + k
+
+def dataloader(folder, radius_train, batch_size, ntsteps=1):
+	"""
+	1. Define the bound of spatial domain. Currently assuming a domain proportional to fluid (150, 93, 100) -> (1.5, 0.93, 1.0)
+	2. Scale data using minmax scalar
+	3. Split into input-output pair
+	4. Split into train and validation index
+	"""
+	data = generateDatasetFluid(folder,splitLen=ntsteps)
+
+	scaler = MinMaxScaler(feature_range=(0,1))
+	scaler, vorticity = data.scaling(scaler)
+	splitData = data.splitDataset()
+
+	num_samples = splitData.shape[0]
+	print("Num of samples batches, timesteps per sample : ", num_samples, splitData.shape[1])
+	indices = np.arange(num_samples)
+	np.random.shuffle(indices)
+	val_split = 0.2
+	num_val_samples = int(num_samples * val_split)
+	val_indices = indices[:num_val_samples]
+	train_indices = indices[num_val_samples:]
+
+	mesh = CartesianMeshGenerator(real_space=[[0, 1.5],[0, 0.93],[0, 1.0]],mesh_size=[150, 93, 100],data=splitData)
+	grid = mesh.get_grid()
+	edge_index = mesh.ball_connectivity(radius_train)
+
+	data_train = []
+	for j in range(num_samples):
+		edge_attr = mesh.attributes(j)
+
+		data_train.append(Data(
+			x=splitData[j,0,:],
+			y=splitData[j,:,:],
+			edge_index=edge_index,
+			edge_attr=torch.tensor(edge_attr, dtype=torch.float32)
+		))
+
+
+	print('train grid', grid.shape, 'edge_index', edge_index.shape, 'edge_attr', edge_attr.shape)
+
+	train_loader = DataLoader([data_train[i] for i in train_indices], batch_size=batch_size, shuffle=True)
+	val_loader = DataLoader([data_train[i] for i in val_indices], batch_size=batch_size, shuffle=False)
+
+	return None, None, None #train_loader, val_loader, scaler
+
+def main(checkpoint_path=None):
+	set_seed(42)
+
+	# Parameters
+	radius_train = 0.012
+	batch_size = 5
+	width = 64  # uplifting node_features+time_emb_dim to wwidth
+	ker_width = 8  
+	edge_features = 12
+	node_features = 6
+	nLayers = 2
+	epochs = 10
+	learning_rate = 0.001 
+	scheduler_step = 500  
+	scheduler_gamma = 0.5
+	ntsteps = 3
+	time_emb_dim = 32
+
+	validation_frequency = 100
+	save_frequency = 100
+
+	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+	# Load data
+	train_loader, val_loader, scaler = dataloader('./sample_data/', radius_train, batch_size, ntsteps)
+	print("----Loaded Data----")
+
+	# # Initialize model
+	# model_instance = SpecGNO(inNodeFeatures=node_features, nNodeFeatEmbedding=width, ker_width=ker_width, nConvolutions=nLayers, nEdgeFeatures=edge_features, ntsteps=ntsteps,time_emb_dim=time_emb_dim).to(device)
+	# print(model_instance)
+	# optimizer = torch.optim.Adam(model_instance.parameters(), lr=learning_rate)
+	# scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
+	# criterion = torch.nn.MSELoss()
+
+	# # Initialize training
+	# start_epoch = 0
+	# if checkpoint_path:
+	# 	checkpoint = torch.load(checkpoint_path)        
+	# 	model_instance.load_state_dict(checkpoint['model_state_dict'])
+	# 	optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+	# 	scheduler.load_state_dict(checkpoint['scheduler_state_dict'])        
+	# 	start_epoch = checkpoint['epoch'] + 1
+	# 	best_val_loss = checkpoint['val_loss']
+	# 	print(f"Resuming training from epoch {start_epoch}")
+	# else:
+	# 	best_val_loss = float('inf')
+
+	# #training
+	# for epoch in range(start_epoch, epochs):
+	# 	model_instance.train()
+	# 	train_loss = 0.0
+	# 	for batch in train_loader:
+	# 		optimizer.zero_grad()
+	# 		batch = batch.to(device)
+	# 		out = model_instance(batch)
+	# 		loss = criterion(out.view(-1, 1), batch.y.view(-1, 1))
+	# 		loss.backward()
+	# 		optimizer.step()
+	# 		train_loss += loss.item()
+		
+	# 	avg_train_loss = train_loss / len(train_loader)
+	# 	print(f"Epoch {epoch+1}/{epochs}, Train Loss: {avg_train_loss:.6f}, lr: {optimizer.param_groups[0]['lr']:.6f}")
+
+	# 	# Save model 
+	# 	if (epoch + 1) % save_frequency == 0:
+	# 		torch.save({
+	# 			'epoch': epoch,
+	# 			'model_state_dict': model_instance.state_dict(),
+	# 			'optimizer_state_dict': optimizer.state_dict(),
+	# 			'scheduler_state_dict': scheduler.state_dict(),
+	# 			'train_loss': avg_train_loss,
+	# 			'val_loss': best_val_loss
+	# 		}, f'model_epoch_{epoch+1}.pth')
+	# 		print(f"Model saved at epoch {epoch+1}")
+
+	# 	# Validation
+	# 	if (epoch + 1) % validation_frequency == 0:
+	# 		model_instance.eval()
+	# 		val_loss = 0.0
+	# 		all_predictions = []
+	# 		all_true_values = []
+	# 		with torch.no_grad():
+	# 			for batch in val_loader:
+	# 				batch = batch.to(device)
+	# 				out = model_instance(batch)
+	# 				loss = criterion(out.view(-1, 1), batch.y.view(-1, 1))
+	# 				val_loss += loss.item()
+	# 				all_predictions.append(out.cpu().numpy())
+	# 				all_true_values.append(batch.y.cpu().numpy())
+
+	# 		avg_val_loss = val_loss / len(val_loader)
+	# 		print(f"Epoch {epoch+1}/{epochs}, Validation Loss: {avg_val_loss:.6f}")
+
+	# 		# Calculate MAE and RMSE
+	# 		all_predictions = np.concatenate(all_predictions, axis=0)
+	# 		all_true_values = np.concatenate(all_true_values, axis=0)
+
+	# 		# Inverse transform predictions and true values
+	# 		predictions_original_scale = all_predictions.reshape(-1,1) #scaler.inverse_transform(all_predictions.reshape(-1, 1))
+	# 		true_values_original_scale = all_true_values.reshape(-1,1) #scaler.inverse_transform(all_true_values.reshape(-1, 1))
+
+	# 		mae = mean_absolute_error(true_values_original_scale, predictions_original_scale)
+	# 		rmse = np.sqrt(mean_squared_error(true_values_original_scale, predictions_original_scale))
+
+	# 		print(f"Validation MAE: {mae:.4f}")
+	# 		print(f"Validation RMSE: {rmse:.4f}")
+
+	# 		# Save best model
+	# 		if avg_val_loss < best_val_loss:
+	# 			best_val_loss = avg_val_loss
+	# 			torch.save(model_instance.state_dict(), 'best_model.pth')
+	# 			print(f"Best model saved with validation loss: {best_val_loss:.6f}")
+
+	# 		# Save best model
+	# 		if avg_val_loss < best_val_loss:
+	# 			best_val_loss = avg_val_loss
+	# 			torch.save(model_instance.state_dict(), 'best_model.pth')
+	# 			print(f"Best model saved with validation loss: {best_val_loss:.6f}")
+
+	# # Final evaluation
+	# model_instance.load_state_dict(torch.load('best_model.pth'))
+	# model_instance.eval()
+	# all_predictions = []
+	# with torch.no_grad():
+	# 	for batch in val_loader:
+	# 		batch = batch.to(device)
+	# 		out = model_instance(batch)
+	# 		all_predictions.append(out.cpu().numpy())
+
+	# all_predictions = np.concatenate(all_predictions, axis=0)
+	# # predictions_original_scale = scaler.inverse_transform(all_predictions.reshape(-1, 1))
+	# predictions_final = all_predictions#predictions_original_scale.reshape(-1, 30, 60).transpose(0, 2, 1)
+
+	# np.save("predictions.npy", predictions_final)
+	# print("Predictions saved as 'predictions.npy'")
+
+if __name__ == "__main__":
+	main()
+	# main('model_epoch_200.pth')
---
+++ model/spectralGraphNetwork.py
@@ -0,0 +1,78 @@
+import torch
+import numpy as np
+import scipy.io
+import h5py
+import sklearn.metrics
+from torch_geometric.data import Data
+import torch.nn as nn
+from model.GKN import *
+from model.spectral import *
+from scipy.ndimage import gaussian_filter
+import os
+import re
+
+class SpecGNO(nn.Module):
+  def __init__(self, inNodeFeatures, nNodeFeatEmbedding, ker_width, nConvolutions, nEdgeFeatures, ntsteps, nModesFNO=1, activation=nn.SiLU(), num_modes=2, time_emb_dim=32):
+    super(SpecGNO, self).__init__()
+
+    self.nConvolutions = nConvolutions
+    self.ntsteps = ntsteps
+    self.time_emb_dim = time_emb_dim
+    self.nNodeFeatEmbedding = nNodeFeatEmbedding
+    self.inNodeFeatures = inNodeFeatures
+
+    #Projecting node feature to higher dimensional embeddings
+    self.feature_embedding = nn.Linear(inNodeFeatures+time_emb_dim, nNodeFeatEmbedding)
+
+    #initialize kernel
+    kernel = DenseNet([nEdgeFeatures, ker_width, ker_width, nNodeFeatEmbedding**2], torch.nn.ReLU)
+
+    #initializeGNO
+    self.layer = NNConv(nNodeFeatEmbedding, nNodeFeatEmbedding, kernel, aggr='mean')
+
+    #initialize spectral layers for features, here we might not need node update
+    self.time_conv_modules = nn.ModuleList() 
+    self.time_conv_x_modules = nn.ModuleList()
+    for _ in range(self.nConvolutions):
+      self.time_conv_modules.append(TimeConv(nNodeFeatEmbedding, nNodeFeatEmbedding, nModesFNO, activation, with_nin=False))
+      self.time_conv_x_modules.append(TimeConv_x(2, 2, num_modes, activation, with_nin=False))
+
+    #bringing higher-dimensional embedding to original feature dimension
+    self.inv_embedding = nn.Linear(nNodeFeatEmbedding, inNodeFeatures)
+    
+  def forward(self, data):
+    x, edge_index, edge_attr, batch, ptr = data.x, data.edge_index, data.edge_attr, data.batch, data.ptr
+    # print(x.shape, edge_attr.shape, edge_index.shape) #torch.Size([3514, 6]) torch.Size([23854, 12]) torch.Size([2, 23854])
+    
+    num_graphs = len(ptr) - 1 # or number of batches
+    num_nodes = x.shape[0]
+    num_edges = edge_index.shape[1]
+    
+    time_emb = get_timestep_embedding(torch.arange(self.ntsteps).to(x), embedding_dim=self.time_emb_dim, max_positions=10000)
+
+    x = x.repeat(self.ntsteps,1)
+    time_emb_repeated = time_emb.unsqueeze(1).repeat(1, num_nodes, 1).view(-1, self.time_emb_dim)
+    x = torch.cat((x, time_emb_repeated), dim=1)
+
+    edge_index = edge_index.repeat(1, self.ntsteps)
+    offsets = torch.arange(self.ntsteps, device=edge_index.device).repeat_interleave(num_edges) * num_nodes
+    edge_index += offsets.unsqueeze(0)
+
+    edge_attr = edge_attr.repeat(self.ntsteps, 1)
+
+    print(x.shape, edge_attr.shape, edge_index.shape) #torch.Size([10542, 38]) torch.Size([71562, 12]) torch.Size([2, 71562])
+
+    x = self.feature_embedding(x)
+
+    for i in range(self.nConvolutions):
+      # edge_attr = self.time_conv_modules[i](edge_attr)
+      x = self.time_conv_modules[i](x.view(self.ntsteps, num_nodes, self.nNodeFeatEmbedding)).view(self.ntsteps*num_nodes, self.nNodeFeatEmbedding)
+      x = F.relu(self.layer(x, edge_index, edge_attr))
+
+    x = self.inv_embedding(x)
+
+    x = x.view(self.ntsteps, num_graphs, num_nodes//num_graphs, self.inNodeFeatures)
+    x = x.transpose(0, 1).reshape(-1, num_nodes // num_graphs, self.inNodeFeatures)
+
+    return x
+  
